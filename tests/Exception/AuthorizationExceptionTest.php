35cde66feceada365f25370b2d64b3673ba20f13
Ondrej Machulda <ondrej.machulda@lmc.eu>
2017-11-10 15:17:13 +0100

    Add basic exceptions tree

---
+++ tests/Exception/AuthorizationExceptionTest.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+namespace Lmc\Matej\Exception;
+
+use Fig\Http\Message\StatusCodeInterface;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
+use PHPUnit\Framework\TestCase;
+
+class AuthorizationExceptionTest extends TestCase
+{
+    /** @test */
+    public function shouldCreateExceptionFromJsonResponse(): void
+    {
+        $request = new Request('GET', 'http://foo.com/endpoint');
+
+        $response = new Response(
+            StatusCodeInterface::STATUS_UNAUTHORIZED,
+            ['Content-Type' => 'application/json'],
+            '{"message": "Invalid signature. Check your secret key","result": "ERROR"}'
+        );
+
+        $exception = AuthorizationException::createFromRequestAndResponse($request, $response);
+
+        $this->assertInstanceOf(AuthorizationException::class, $exception);
+        $this->assertSame(
+            'Matej API authorization error for url "/endpoint" (Invalid signature. Check your secret key)',
+            $exception->getMessage()
+        );
+        $this->assertSame(StatusCodeInterface::STATUS_UNAUTHORIZED, $exception->getCode());
+        $this->assertSame($request, $exception->getRequest());
+        $this->assertSame($response, $exception->getResponse());
+    }
+}
---
+++ tests/Exception/RequestExceptionTest.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+
+namespace Lmc\Matej\Exception;
+
+use Fig\Http\Message\StatusCodeInterface;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
+use PHPUnit\Framework\TestCase;
+
+class RequestExceptionTest extends TestCase
+{
+    /** @test */
+    public function shouldConstructNewException(): void
+    {
+        $message = 'Foo bar baz message';
+        $request = new Request('GET', 'http://foo.com');
+        $response = new Response(StatusCodeInterface::STATUS_NOT_FOUND);
+
+        $exception = new RequestException($message, $request, $response);
+
+        $this->assertSame($message, $exception->getMessage());
+        $this->assertSame(StatusCodeInterface::STATUS_NOT_FOUND, $exception->getCode());
+        $this->assertSame($request, $exception->getRequest());
+        $this->assertSame($response, $exception->getResponse());
+    }
+}
